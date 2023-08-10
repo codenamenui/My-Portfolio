f228041dd0911d14d4df5a8a7479ec69980c2b8b
bando <codenamenui@gmail.com>
2023-08-10 14:43:38 +0800

    spread

--- main.js
+++ main.js
@@ -4,7 +4,20 @@ let diamond = document.querySelector("#diamond");
 let diaPath = document.querySelector("#filledDia");
 let diaNav = document.getElementsByClassName("diamond-nav")[0];
 let navText = document.querySelectorAll("nav a");
+let navH6 = document.querySelectorAll("nav a h6");
 let openNav = true;
+
+for (let i = 0; i < navText.length; i++) {
+    navH6[i].classList.toggle('hover-line');
+    navText[i].addEventListener("mouseenter", () => {
+        navH6[i].classList.toggle('hover-line')
+        navH6[i].classList.toggle('unhover-line');
+    });
+    navText[i].addEventListener("mouseleave", () => {
+        navH6[i].classList.toggle('hover-line');
+        navH6[i].classList.toggle('unhover-line');
+    });
+}
 
 diamond.addEventListener('click', () => {
     if (!openNav) 
@@ -30,10 +43,10 @@ diamond.addEventListener('click', () => {
         leftArrow.style.opacity = "1";
         diaNav.style.gap = "clamp(0.5vw, 2vw, 3vw)";
         diaPath.style.opacity = "1";
-        navText[0].style.transform = "translateX(-200%)";
-        navText[1].style.transform = "translateX(-100%)";
-        navText[2].style.transform = "translateX(100%)";
-        navText[3].style.transform = "translateX(200%)";
+        navText[0].style.transform = "translateX(-300%)";
+        navText[1].style.transform = "translateX(-150%)";
+        navText[2].style.transform = "translateX(150%)";
+        navText[3].style.transform = "translateX(300%)";
         for (let text of navText) {
             text.style['transition'] = "opacity 300ms linear, transform 300ms linear";
             text.style.opacity = "1";
